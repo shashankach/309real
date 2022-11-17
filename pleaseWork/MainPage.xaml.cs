d55e9c66bc99b6169f8b065e82708e68b3021505
shashankach <74078700+shashankach@users.noreply.github.com>
2022-11-17 20:15:01 +0100

    com

---
+++ pleaseWork/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace pleaseWork;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ pleaseWork/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace pleaseWork;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
