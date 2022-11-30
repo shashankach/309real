44bd3e245e78109271eb126c5fdd507048b4ae6e
shashankach <74078700+shashankach@users.noreply.github.com>
2022-11-30 14:44:26 +0100

    map

--- pleaseWork/MauiProgram.cs
+++ pleaseWork/MauiProgram.cs
@@ -15,7 +15,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-		builder.Services.AddMudServices();
+        
+
+        builder.Services.AddMudServices();
 		builder.Services.AddMauiBlazorWebView();
 		#if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
