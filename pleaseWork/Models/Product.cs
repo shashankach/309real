21838b5331097bafaf23f6a250d9aeea4d04d2cb
joseph-macfarlane <josephmmacfarlane@gmail.com>
2022-12-05 21:24:25 +0100

    same

---
+++ pleaseWork/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pleaseWork.Models
+{
+    public class Product
+    {
+
+        [JsonProperty(PropertyName = "name")]
+        public string name { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public Double price { get; set; }
+
+        [JsonProperty(PropertyName = "seller")]
+        public string seller { get; set; }
+
+        [JsonProperty(PropertyName = "image")]
+        public string image { get; set; }
+    }
+}
