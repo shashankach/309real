58c4cc0a2dcfdd58f81e3229ed717ae1c7016d3c
shashankach <74078700+shashankach@users.noreply.github.com>
2022-12-06 19:31:15 +0100

    fixed accouts

---
+++ pleaseWork/Data/Userd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace pleaseWork.Data
+{
+	public class Userd
+	{
+		
+	
+            [JsonProperty(PropertyName = "UsernameTextValue")]
+            public string UsernameTextValue { get; set; }
+
+            [JsonProperty(PropertyName = "PasswordTextValue")]
+            public string PasswordTextValue{ get; set; }
+
+            [JsonProperty(PropertyName = "SelectedOption")]
+            public string SelectedOption { get; set; }
+           
+            [JsonProperty(PropertyName = "NameTextValue")]
+            public string NameTextValue { get; set; }
+
+            [JsonProperty(PropertyName = "EmailTextValue")]
+            public string EmailTextValue { get; set; }
+    
+	}
+}
+
